27fe0858cd2a1305d69696d36f304964b7c98c80
gizmore <gizmore@wechall.net>
2023-04-06 09:54:56 +0200

    '7.0.2-r1771'

--- GDT_EmailFormat.php
+++ GDT_EmailFormat.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace GDO\Mail;
 
 use GDO\Core\GDT_Enum;
@@ -6,21 +7,22 @@ use GDO\Core\GDT_Enum;
 /**
  * Enum that switches between text und html format.
  *
- * @version 7.0.1
+ * @version 7.0.3
  * @since 5.0
  * @author gizmore
  */
 final class GDT_EmailFormat extends GDT_Enum
 {
 
-	public const TEXT = 'text';
-	public const HTML = 'html';
+	final public const TEXT = 'text';
+	final public const HTML = 'html';
 
 	protected function __construct()
 	{
 		parent::__construct();
 		$this->icon('format');
 		$this->enumValues(self::TEXT, self::HTML);
+		$this->notNull();
 	}
 
 	public function defaultLabel(): self
