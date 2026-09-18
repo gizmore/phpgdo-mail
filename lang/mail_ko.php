f159aac47bd4a84c1d996b4e7a501a20700ca581
gizmore <gizmore@wechall.net>
2026-09-18 04:08:53 +0200

    'more korean'

---
+++ lang/mail_ko.php
@@ -0,0 +1,43 @@
+<?php
+namespace GDO\Mail\lang;
+
+return [
+	'cfg_user_allow_email' => '사람들이 나에게 이메일을 보낼 수 있도록 허용하시겠습니까?',
+	'mt_mail_send' => '메일 보내기',
+	'info_send_mail' => '여기에서 사용자에게 메일을 보낼 수 있습니다. 귀하의 이메일이 그들에게 알려질 것입니다.',
+	'info_need_mail' => '다른 사용자에게 메일을 보내려면 자신의 이메일 주소가 필요합니다.',
+	'err_user_does_not_want_mail' => '이 사용자는 귀하의 이메일을 원하지 않습니다. 이것은 그들의 일반적인 설정일 수 있습니다.',
+	'err_user_does_not_has_mail' => '이 사용자는 이메일 주소가 없습니다.',
+	'err_arbr_mail_self' => '이 방법으로는 자신에게 이메일을 보낼 수 없습니다.',
+	'msg_arbr_mail_sent' => '메일이 %s(으)로 성공적으로 전송되었습니다.',
+	'mail_send_arbr_subj' => '[%s] %s - %s에서',
+	'mail_send_arbr_body' => '친애하는 %s님,
+
+%s님이 %s을(를) 통해 메일을 보냈습니다.
+
+제목: %s
+메시지:
+%s
+
+진심으로,
+%3$s 시스템',
+	'mt_mail_change' => '이메일 변경',
+	'md_mail_change' => '이메일 주소 변경',
+	'msg_mail_changing' => '이메일을 `%s`(으)로 변경하려고 합니다. 이것을 확인해 주십시오.',
+	'mt_mail_validate' => '이메일 검증',
+	'md_mail_validate' => '이메일 주소를 확인하세요',
+	'email_confirmed' => '확인된 시간',
+	'msg_mail_confirmed' => '귀하의 이메일이 확인되었으며 \'%s\'(으)로 설정되었습니다.',
+	'mailt_confirm_email' => '%s: 이메일을 확인하세요',
+	'mailb_confirm_email' => '안녕하세요 %s님,
+
+다음 링크를 방문하여 이메일 주소를 확인하세요.
+
+%s
+
+진심으로,
+%s 팀',
+	'cfg_allow_email' => '사용자에 대한 이메일 전송 허용',
+	'mt_mail_requestvalidation' => '이메일 검증',
+	'info_email_request_validation' => '여기에서 계정 이메일을 설정할 수 있습니다.',
+];
